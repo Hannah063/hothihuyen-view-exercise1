4fb051fad231822931ffa713a870e09a53c1419e
Huyen Ho <hothihuyen06031195@gmail.com>
2024-03-06 16:46:56 +0700

    layout 10%

--- resources/views/taskList.blade.php
+++ resources/views/taskList.blade.php
@@ -4,18 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Task list</title>
+    <link rel="stylesheet" href="{{asset('assets/clients/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/clients/css/style.css')}}">
+    <title>HO THI HUYEN</title>
 </head>
 <body>
-    <div class="row">
-        <p class="border-1">Task list</p>
+    <div class="row md-5">
+        <p>Task list</p>
     </div>
-    @section('css')
-        
-    @endsection
-    @section('js')
-        
-    @endsection
+    <div class="row col-12 mb-5">
+        <div class="col-3"></div>
+        <div class="card col-6 p-0">
+            <div class="card-header ">
+                New task
+            </div>
+            <div class="card-body">
+                <label for="task">Task</label>
+                <input type="text" class="">
+                <button class="btn btn-outline-dark">Add task</button>
+            </div>
+        </div>
+    </div>
+    <div class="row col-12">
+        <div class="col-3"></div>
+        <div class="card col-6 p-0">
+            <div class="card-header">
+                <p>New task</p>
+            </div>
+            <div class="card-body">
+                <label for="task">Task</label>
+                <input type="text">
+            </div>
+        </div>
+    </div>
+
 </body>
+<script src="{{asset('assets/clients/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/clients/js/custom.js')}}"></script>
 </html>
