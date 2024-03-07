032a05ca419087c08897ca4a1d3d4fc35795171a
Huyen Ho <hothihuyen06031195@gmail.com>
2024-03-07 22:56:09 +0700

    done

--- resources/views/taskList.blade.php
+++ resources/views/taskList.blade.php
@@ -9,31 +9,78 @@
     <title>HO THI HUYEN</title>
 </head>
 <body>
-    <div class="row md-5">
-        <p>Task list</p>
+    <div class="row md-5 mt-4">
+        <div class="col-4"></div>
+        <div class="col-4 alert alert-secondary" style="display: flex; justify-content: center; align-items: center">
+            <p class="m-0" style="font-size: 25px; padding-right: 350px">
+                Task list
+            </p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20" height="20"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        </div>
     </div>
-    <div class="row col-12 mb-5">
-        <div class="col-3"></div>
-        <div class="card col-6 p-0">
-            <div class="card-header ">
-                New task
-            </div>
-            <div class="card-body">
-                <label for="task">Task</label>
-                <input type="text" class="">
-                <button class="btn btn-outline-dark">Add task</button>
+    <div class="row col-12 mb-5 ">
+        <div class="col-4" style="margin-right: 10px"></div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-header ">
+                    New task
+                </div>
+                <div class="card-body">
+                    <div class="pb-3"> 
+                        <b>Task</b>
+                    </div>
+                    <div class="pb-3">
+                        <input type="text" style="width: 100%; border-radius: 5px">
+                    </div>
+                    <div>
+                        <button class="btn btn-outline-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20" height="20"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                            Add task
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     <div class="row col-12">
-        <div class="col-3"></div>
-        <div class="card col-6 p-0">
-            <div class="card-header">
-                <p>New task</p>
-            </div>
-            <div class="card-body">
-                <label for="task">Task</label>
-                <input type="text">
+        <div class="col-4" style="margin-right: 10px"></div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-header">
+                    <p>Current Tasks</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th><b>Task</b></th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <td>First task</td>
+                            <td>                                                          
+                                <button type="button" class="btn btn-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16">
+                                        <!-- Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                                        <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" fill="#ffffff"/>
+                                    </svg> 
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Second task</td>
+                            <td>                                                          
+                                <button type="button" class="btn btn-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16">
+                                        <!-- Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                                        <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" fill="#ffffff"/>
+                                    </svg> 
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
